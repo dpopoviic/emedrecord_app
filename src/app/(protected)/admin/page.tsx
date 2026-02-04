6ba6f5a306613dd6e8449a6f690f87d152282806
Dragana Popović <dp20220322@student.fon.bg.ac.rs>
2026-02-04 12:47:34 +0100

    popravljen kod, setup za clerk

--- src/app/(protected)/admin/page.tsx
+++ src/app/(protected)/admin/page.tsx
@@ -1,11 +1,40 @@
 
 import { Button } from "@/components/ui/button";
-import { BriefcaseBusiness, BriefcaseMedical, User, Users } from "lucide-react";
+import { getRole } from "@/utils/roles";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { BriefcaseBusiness, BriefcaseMedical, Link, User, Users } from "lucide-react";
 import React from "react";
 
+ const { userId } = await auth();
+   const role = await getRole();
+
 const AdminDashboard = () => {
-    return<></>
-};
+    return (<div className="flex gap-4">
+            {userId ? (
+              <>
+                <UserButton/>
+              </>
+            ) : (
+              <>
+                <Link href="/sign-up">
+                  <Button className="md:text-base font-light">
+                    New Patient
+                  </Button>
+                </Link>
+
+                <Link href="/sign-in">
+                  <Button
+                    variant="outline"
+                    className="md:text-base underline hover:text-blue-600"
+                  >
+                    Login to account
+                  </Button>
+                </Link>
+              </>
+            )}
+          </div>
+);};
 
   
 export default AdminDashboard;
